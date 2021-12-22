d36a6d76713026e0fae75ea6447a75aba3707fda
allwyns <allwyns@axioned.com>
2021-12-22 16:43:09 +0530

    add: audio tracks are added and set audio play on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
 var lists = document.querySelectorAll("li");
+var sounds = document.querySelector(".sound");
+var audio_lists = ['applause.mp3','Boo.mp3','gasp.mp3','Tada.mp3','victory.mp3','wrong.mp3'];
+var dom_audio = document.createElement('audio');
 
 for(let i=0; i < lists.length; i++) {
-    lists[i].addEventListener('mouseover', ()=>{
-        lists[i].style.background = 'rgba(0,0,0,0.2)'
-    });
-
-    lists[i].addEventListener('mouseout', ()=>{
-        lists[i].style.background = 'rgba(0,0,0,0.3)'
+    lists[i].addEventListener('click', function () {
+        dom_audio.src = "assets/vendor/"+audio_lists[i];
+        dom_audio.setAttribute("autoplay", "");
+        dom_audio.setAttribute("muted", "");
+        dom_audio.muted = false;
+        console.log(dom_audio);
     });
 }
